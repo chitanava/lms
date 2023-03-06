7ae9e41c3277caac7bf38c6b926d98f86ddeb31d
Nika Chitanava <chitanava@gmail.com>
2023-03-06 22:15:02 +0400

    refactoring dropdown component

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -13,20 +13,21 @@
   @yield('breadcrumbs')
 
   <div class="flex-1 flex justify-end">
-    <div x-data="{ open:false }" class="flex relative flex-1 justify-end">
-      <button x-on:click="open = !open"
-        class="w-10 h-10 rounded-full text-white bg-slate-900 flex justify-center items-center">
+    <x-admin.dropdown 
+      class="min-w-[250px]" 
+      :data="[
+        ['title' => 'Profile', 'url' => '#', 'icon' => 'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'],
+        ['title' => 'Log out', 'url' => '#', 'icon' => 'M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'],
+      ]">
+      
+      <x-slot:button class="w-10 h-10 rounded-full text-white bg-slate-900 flex justify-center items-center">
         <span class="font-semibold">NC</span>
-      </button>
-      <x-admin.dropdown class="min-w-[250px]" :data="[
-          ['title' => 'Profile', 'url' => 'http://google.com', 'icon' => 'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'],
-          ['title' => 'Log out', 'url' => '#', 'icon' => 'M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'],
-        ]">
-        <div class="py-3 px-5 border-b border-b-slate-200">
-          <p class="font-semibold">Nika Chitanava</p>
-          <p class="text-xs text-slate-400">chitanava@gmail.com</p>
-        </div>
-      </x-admin.dropdown>
-    </div>
+      </x-slot:button>
+      
+      <x-slot:header>
+        <p class="font-semibold">Nika Chitanava</p>
+        <p class="text-xs text-slate-400">chitanava@gmail.com</p>
+      </x-slot:header>
+    </x-admin.dropdown>
   </div>
 </header>
--- resources/views/components/admin/dropdown.blade.php
+++ resources/views/components/admin/dropdown.blade.php
@@ -1,20 +1,31 @@
 @props([
-  'data' => [],
+  'button',
+  'header' => null,
+  'data' => []
 ])
 
-<div 
-  x-transition x-on:click.outside="open = false" x-show="open"
-  {{ $attributes->merge(['class' => 'absolute top-full right-0 mt-1 bg-white rounded-lg border-slate-200 border shadow-md']) }}
-  x-cloak>
-  {{ $slot }}
+<div x-data="{ open:false }" class="relative">
+  <button x-on:click="open = !open" {{ $button->attributes }}>{{ $button }}</button>
+  <div 
+    x-transition 
+    x-on:click.outside="open = false" 
+    x-show="open"
+    {{ $attributes->merge(['class' => 'absolute top-full right-0 mt-1 bg-white rounded-lg border-slate-200 border shadow-md']) }}
+    x-cloak>
+      
+    @if ($header)        
+    <header class="py-3 px-5 border-b border-b-slate-200">
+      {{ $header }}
+    </header>
+    @endif
 
-  <ul class="p-1">
-    @foreach ($data as $item)
-    <x-admin.dropdown-menu-item 
-      :title="$item['title']"
-      :icon="$item['icon']" 
-      :href="$item['url']"
-      />
-    @endforeach
-  </ul>
+    <ul class="p-1">
+      @foreach ($data as $item)
+      <x-admin.dropdown-menu-item 
+        :title="$item['title']"
+        :icon="$item['icon']"
+        :href="$item['url']" />
+      @endforeach
+    </ul>
+  </div>
 </div>
