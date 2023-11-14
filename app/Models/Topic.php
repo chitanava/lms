945aaf5b69e362fb4fdbcbce5b87b044cf3f83cf
Nika Chitanava <chitanava@gmail.com>
2023-11-14 19:41:34 +0400

    Add Topic model and migration

---
+++ app/Models/Topic.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class);
+    }
+}
