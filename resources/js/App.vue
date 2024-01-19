5994da7888215dd3bd0daf4c87acf843419693b2
Nika Chitanava <chitanava@gmail.com>
2024-01-19 15:42:25 +0400

    Add Vue

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <ul class="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box mb-4">
+            <li><router-link to="/">Go to Home</router-link></li>
+            <li><router-link to="/about">Go to About</router-link></li>
+        </ul>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+</script>
+
+<template>
+    <div class="card w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews <span class="font-bold">{{store.count}}</span> shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary" @click="store.increment()">Buy Now</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
