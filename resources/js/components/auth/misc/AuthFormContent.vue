32babd7e36dbd1f52e39a7ff52af0fb7824a2a7b
Nika Chitanava <chitanava@gmail.com>
2024-01-31 00:08:07 +0400

    Conditionally hide <form> tag in AuthFormContent

--- resources/js/components/auth/misc/AuthFormContent.vue
+++ resources/js/components/auth/misc/AuthFormContent.vue
@@ -3,15 +3,22 @@ const props = defineProps({
     novalidate: {
         type: Boolean,
         default: false
+    },
+    applyForm: {
+      type: Boolean,
+      default: true
     }
 })
 const emit = defineEmits(['submit'])
 </script>
 
 <template>
-    <form @submit.prevent="$emit('submit')" :novalidate="props.novalidate">
+    <form v-if="props.applyForm" @submit.prevent="$emit('submit')" :novalidate="props.novalidate">
         <div class="space-y-6">
             <slot />
         </div>
     </form>
+    <div v-else class="space-y-6">
+      <slot />
+    </div>
 </template>
